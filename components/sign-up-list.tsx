1b27890fafeeb0691aa1a8f9b437078ce8e64f9a
Devin Clark <devin.clark27@gmail.com>
2024-10-26 16:40:32 -0500

    chore: spiders and broken prod

--- components/sign-up-list.tsx
+++ components/sign-up-list.tsx
@@ -16,9 +16,11 @@ import {
 } from "@/components/ui/drawer";
 import { Signup } from "@prisma/client";
 import { ScrollingText } from "./scrolling-text";
+import Image from "next/image";
 
 export const SignUpList = ({ signups }: { signups: Signup[] }) => {
   const [isOpen, setIsOpen] = useState(false);
+
   const currentSong = signups[0];
   const upcomingSongs = signups.slice(1);
 
@@ -30,16 +32,30 @@ export const SignUpList = ({ signups }: { signups: Signup[] }) => {
           className="w-full p-4 h-24 rounded-none border-t"
         >
           <div className="flex flex-row justify-between w-full items-center gap-4">
-            <div className="flex flex-col items-start flex-grow">
-              <div className="flex flex-row justify-between w-full items-center font-bold text-lg">
-                <span>Up Now</span>
-                <span>{currentSong.name}</span>
-              </div>
+            {currentSong && (
+              <div className="flex flex-col items-start flex-grow">
+                <div className="flex flex-row justify-between w-full items-center font-bold text-lg">
+                  <span>Up Now</span>
+                  <span>{currentSong.name}</span>
+                </div>
 
-              <ScrollingText
-                text={`${currentSong.song} by ${currentSong.artist}`}
-              />
-            </div>
+                <ScrollingText
+                  text={`${currentSong.song} by ${currentSong.artist}`}
+                />
+              </div>
+            )}
+            {!currentSong && (
+              <>
+                <span> {"It's just cobwebs here"}</span>
+                <Image
+                  src={"/spiderweb.png"}
+                  width={96}
+                  height={96}
+                  className="object-cover"
+                  alt="spiderweb"
+                />
+              </>
+            )}
             <ChevronUp className="ml-2 h-4 w-4" />
           </div>
         </Button>
@@ -52,15 +68,28 @@ export const SignUpList = ({ signups }: { signups: Signup[] }) => {
           </DrawerDescription>
         </DrawerHeader>
         <ScrollArea className="h-[50vh] px-4">
-          {upcomingSongs.map((signup) => (
-            <div key={signup.id} className="mb-4 p-4 border rounded-lg">
-              <h3 className="font-semibold flex flex-row gap-1 items-center">
-                <MicIcon className="size-4" />
-                {signup.name}
-              </h3>
-              <ScrollingText text={`${signup.song} by ${signup.artist}`} />
+          {upcomingSongs.length > 0 &&
+            upcomingSongs.map((signup) => (
+              <div key={signup.id} className="mb-4 p-4 border rounded-lg">
+                <h3 className="font-semibold flex flex-row gap-1 items-center">
+                  <MicIcon className="size-4" />
+                  {signup.name}
+                </h3>
+                <ScrollingText text={`${signup.song} by ${signup.artist}`} />
+              </div>
+            ))}
+          {upcomingSongs.length === 0 && (
+            <div className="flex flex-col justify-center items-center">
+              <span>No signups, be the first!</span>
+              <Image
+                src={"/spiderweb.png"}
+                width={384}
+                height={384}
+                className="size-96"
+                alt="spiderweb"
+              />
             </div>
-          ))}
+          )}
         </ScrollArea>
         <DrawerFooter>
           <DrawerClose asChild>
